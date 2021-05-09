6211dcac3417d9fca4ab5aaef28cbca4caf438f1
BaoDNguyen <54602191+BaoDNguyen@users.noreply.github.com>
2021-05-09 00:53:30 -0500

    add data selection

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
-
 viz = vizControl();
-d3.csv('./data/concept_score.csv').then(file => {
-    readFile(file);
-    normalization();
-    compute_metric();
+function main (fileName) {
+    d3.csv(fileName).then(file => {
+        readFile(file);
+        normalization();
+        compute_metric();
 
-    // UI init
-    initMenu();
-    viz.init()
-        .feature(concepts)
-        .data({data,dataArr})
-        .draw();
-    updateProcess()
-});
+        // UI init
+        initMenu();
+        viz.init()
+            .feature(concepts)
+            .data({data,dataArr})
+            .draw();
+        updateProcess()
+    });
+}
+
+main('./data/concept_score.csv');
